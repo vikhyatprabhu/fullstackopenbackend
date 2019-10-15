4a677d241c022158626f84164374b2342aea4e2e
Vikhyat Prabhu <vikhyat.prabhu@gmail.com>
2019-10-15 07:15:44 +0530

    Added solutions till 3.6

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "Vikhyat",
+    "number": "123124",
+    "id": 5
+  }
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+
+app.get("/info",(req,res)=>{
+  let output="Phonebook has "+persons.length+" entries ";
+   output=output.concat("\n\n"+new Date().toUTCString());
+   res.write(output);
+   res.end();
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+
+const generateId = () => {
+  return Math.floor(Math.random()*Math.floor(100000))
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const person = {
+    name:body.name,
+    number:body.number,
+    id: generateId()
+  }
+  if(persons.map(p=>p.name).includes(body.name)){
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  persons = persons.concat(person);
+  response.json(person)
+})
+
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
