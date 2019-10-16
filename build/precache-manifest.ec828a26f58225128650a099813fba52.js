c0835ffbc4c214da92b340a63fd79a5a90da3453
Vikhyat Prabhu <vikhyat.prabhu@gmail.com>
2019-10-16 23:09:46 +0530

    Add build folder

---
+++ build/precache-manifest.ec828a26f58225128650a099813fba52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cc41c196e74c3203c851e896c7ba87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c73d827c4cdf0f38187",
+    "url": "/static/css/main.4e7f5d35.chunk.css"
+  },
+  {
+    "revision": "28268a09fae3c255fff5",
+    "url": "/static/js/2.88849929.chunk.js"
+  },
+  {
+    "revision": "2c73d827c4cdf0f38187",
+    "url": "/static/js/main.82e6e5ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
